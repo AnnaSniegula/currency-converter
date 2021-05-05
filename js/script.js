4014cd311b6e9acea763fa38e8623bbf87d00ce0
Anna Sniegula <anna.sniegula@gmail.com>
2021-05-05 21:17:41 +0200

    comments removed

--- js/script.js
+++ js/script.js
@@ -1,47 +1,4 @@
 
-
-// let form = document.querySelector(".js-form");
-// let inputElement = document.querySelector(".js-currencyPLN");
-// let selectCurrency = document.querySelector(".js-selectCurrency");
-// let getResult = document.querySelector(".js-result");
-
-
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-
-//     let rateEUR = 4.55;
-//     let rateUSD = 3.78;
-//     let currency = selectCurrency.value;
-
-//     switch (currency) {
-//         case "usd":
-//             let resultUSD = inputElement.value / rateUSD;
-//             getResult.innerText = resultUSD.toFixed(2) + " USD";
-//             break;
-
-//         case "eur":
-//             let resultEUR = inputElement.value / rateEUR;
-//             getResult.innerText = resultEUR.toFixed(2) + " EUR";
-//             break;
-
-//     }
-//     form.addEventListener("reset", () => {
-//         getResult.innerText = " ";
-//     });
-
-// });
-
-
-
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault();
-
-//     let resultEUR = inputElement.value / rateEUR;
-//     getResult.innerText = resultEUR.toFixed(2) + " EUR";
-
-// });
-
-
 {
 const form = document.querySelector(".js-form");
 const inputElement = document.querySelector(".js-currencyPLN");
@@ -64,7 +21,6 @@ const getResult = document.querySelector(".js-result");
            
     };
 
-    
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
