fbd348b4088c4ed82106fb682124272a7fd4dd8f
Anna Sniegula <anna.sniegula@gmail.com>
2021-05-26 21:02:32 +0200

    const getCurrencyAmount and const init added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 
 {
-    const form = document.querySelector(".js-form");
-    const inputElement = document.querySelector(".js-currencyPLN");
-    const selectCurrency = document.querySelector(".js-selectCurrency");
-    const getResult = document.querySelector(".js-result");
-
-
     const calculateResult = (amount, currency) => {
         const rateEUR = 4.55;
         const rateUSD = 3.78;
@@ -21,20 +15,70 @@
 
     };
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
+    const getResult = document.querySelector(".js-result");
+    const updateTextGetAmount = (result, currency) => {
+        getResult.innerText = `${result.toFixed(2)} ${currency}`
+    };
+
+    const getCurrencyAmount = (e) => {
+        e.preventDefault();
+        const inputElement = document.querySelector(".js-currencyPLN");
+        const selectCurrency = document.querySelector(".js-selectCurrency");
 
         const currency = selectCurrency.value;
         const amount = +inputElement.value;
-
         let result = calculateResult(amount, currency);
+        updateTextGetAmount(result, currency);
+    };
 
-        getResult.innerText = `${result.toFixed(2)} ${currency}`;
+    const clearCurrencyInput = () => { getResult.innerText = " " };
 
-    })
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", getCurrencyAmount);
+        form.addEventListener("reset", clearCurrencyInput);
+    }
 
-    form.addEventListener("reset", () => {
-        getResult.innerText = " ";
+    init ();
 
-    });
+
 }
+
+
+// const form = document.querySelector(".js-form");
+// const inputElement = document.querySelector(".js-currencyPLN");
+// const selectCurrency = document.querySelector(".js-selectCurrency");
+// const getResult = document.querySelector(".js-result");
+
+// const calculateResult = (amount, currency) => {
+//     const rateEUR = 4.55;
+//     const rateUSD = 3.78;
+
+
+//     switch (currency) {
+//         case "USD":
+//             return amount / rateUSD;
+
+//         case "EUR":
+//             return amount / rateEUR;
+//     }
+
+// };
+
+
+    // form.addEventListener("submit", (event) => {
+    //     event.preventDefault();
+
+    //     const currency = selectCurrency.value;
+    //     const amount = +inputElement.value;
+
+    //     let result = calculateResult(amount, currency);
+
+    //     getResult.innerText = `${result.toFixed(2)} ${currency}`;
+
+    // })
+
+    // form.addEventListener("reset", () => {
+    //     getResult.innerText = " ";
+
+    // })
